cff68c8c2402f5f463736294d2349388e6b72035
RuiDavid5b <ruicardoso0512@gmail.com>
2026-08-25 00:55:44 +0100

    remove weekly filings/news in DAG, fetch everything daily

--- airflow/tests/test_daily_pipeline.py
+++ airflow/tests/test_daily_pipeline.py
@@ -31,7 +31,6 @@ class TestDailyPipelineStructure:
             "price_commands",
             "run_price_pipeline",
             "run_digest_pipeline",
-            "is_weekly_run",
             "filings_commands",
             "news_commands",
             "run_filings_pipeline",
@@ -69,12 +68,6 @@ class TestDailyPipelineStructure:
     def test_anomaly_explanations_has_all_done_trigger_rule(self, dag):
         anomaly_task = dag.get_task("run_anomaly_explanations")
         assert anomaly_task.trigger_rule == "all_done"
-
-    def test_weekly_gate_precedes_filings_and_news(self, dag):
-        gate = dag.get_task("is_weekly_run")
-        assert "filings_commands" in gate.downstream_task_ids or any(
-            "filings" in t for t in gate.downstream_task_ids
-        )
 
     def test_dag_has_no_cycles(self, dag):
         dag.check_cycle()
